678b55b33a30b71fe3e18f055ca825b769ba874f
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2020-09-09 11:32:28 -0400

    link reviews with restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
   has_one_attached :image
+
+  has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_133814) do
+ActiveRecord::Schema.define(version: 2020_09_09_143203) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_133814) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
